c9c080f8d35358b1982dece8f67aaa5b29103521
Sady <Sady@DESKTOP-SGDBGV8>
2023-08-12 12:49:07 -0400

    prueba

---
+++ SelJul23/src/test/java/prueba/NewTest.java
@@ -0,0 +1,16 @@
+package prueba;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void tc01() {
+	  System.out.println("HOLA");
+		String exePath = "./driver/chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+  }
+}
